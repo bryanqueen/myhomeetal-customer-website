e0b93b1e972b4862e1ec0e76aa4607fbd10da0a9
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-07-11 23:17:00 +0100

    fixed bug

--- app/components/account/NoHistory.tsx
+++ app/components/account/NoHistory.tsx
@@ -12,7 +12,7 @@ const NoHistory = ({
       <div className='h-16 w-16 rounded-full bg-[#FFC5C6]' />
       <p className='font-clashmd text-lg text-center text-myGray lg:text-2xl'>{title}</p>
       <p className='text-center w-[80%] lg:w-full text-xs text-myGray lg:text-sm'>
-        Looks like you haven't made any purchases with us so far. Ready to start
+        Looks like you haven&#39;t made any purchases with us so far. Ready to start
         your shopping journey? Dive into our collection and discover products
         tailored just for you.
       </p>

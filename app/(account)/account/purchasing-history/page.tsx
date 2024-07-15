50c18909bdfe618dc73428deab51a10a0f0d5ced
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-07-15 23:35:37 +0100

    update

--- app/(account)/account/purchasing-history/page.tsx
+++ app/(account)/account/purchasing-history/page.tsx
@@ -1,19 +1,39 @@
+import PurchasingHistory from '@/app/components/account/PurchasingHistory';
+import Button from '@/app/components/Button';
+import { ArrowLeftIcon } from '@heroicons/react/16/solid';
 import { Metadata } from 'next';
-import { Suspense } from 'react';
-
-import PurchasingHistory from '@components/account/PurchasingHistory';
-import { PageProps } from '@/app/utils/types';
+import { headers } from 'next/headers';
 
 export const metadata: Metadata = {
   title: 'Purchasing History | Myhomeetal',
 };
 
-export default function PurchasingHistoryPage({ searchParams }: PageProps) {
+export default function PurchasingHistoryPage() {
+  const headersList = headers();
+  const previousPath = headersList.get('referer') || '';
   return (
-    <main>
-      <Suspense>
-        <PurchasingHistory searchParams={searchParams} />
-      </Suspense>
+    <main className='px-[3%] lg:px-0'>
+      <div className='flex items-center py-5 pl-1 lg:hidden'>
+        <Button
+          href={previousPath}
+          className='justify-start font-clashmd text-xs text-myGray lg:justify-center lg:font-clash lg:text-sm'
+          linkType='rel'
+          variant='ghost'
+        >
+          <ArrowLeftIcon
+            width={17}
+            className=' mr-[2px] mt-[-1px] lg:mr-1 lg:mt-[-3px]'
+          />
+          Back
+        </Button>
+        <p className='text-center font-clashmd text-xs text-myGray lg:hidden'>
+          My Orders
+        </p>
+      </div>
+      <h1 className='hidden font-clashmd text-3xl text-myGray lg:block'>
+        My Orders
+      </h1>
+      <PurchasingHistory />
     </main>
   );
 }
--- app/components/account/PurchasingHistory.tsx
+++ app/components/account/PurchasingHistory.tsx
@@ -1,114 +1,134 @@
 'use client';
-
-import { useEffect, useState } from 'react';
-import * as Tabs from '@radix-ui/react-tabs';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useState } from 'react';
 import NoHistory from './NoHistory';
-import useQueryParams from '@components/hooks/useQueryParams';
-import { PageProps } from '@/app/utils/types';
-import authUtils from '@/app/utils/authUtils';
-import productService from '@/app/services/productService';
+import Image from 'next/image';
+import cn from 'classnames';
+import ProductPrice from '../product/ProductPrice';
+import { useRegion } from '@/app/RegionProvider';
+import Link from 'next/link';
 
-interface UserInfo {
-  firstname: string;
-  email: string;
-  id: string;
-}
+const ordersData = [
+  {
+    id: '#2349201',
+    product:
+      'Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13 - Light Green',
+    status: 'Completed',
+    price: 2000, // Can be 'Pending', 'Completed', 'Cancelled', etc.
+  },
+  {
+    id: '#2349202',
+    product: 'Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13 - Light Blue',
+    status: 'Pending',
+    price: 2000,
+  },
+  {
+    id: '#2349203',
+    product: 'Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13 - Black',
+    status: 'Completed',
+    price: 2000,
+  },
+  {
+    id: '#2349204',
+    product: 'Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13 - Red',
+    status: 'Ongoing',
+    price: 2000,
+  },
+  {
+    id: '#2349205',
+    product: 'Samsung Galaxy A14 6.6" 4GB RAM/64GB ROM Android 13 - White',
+    status: 'Completed',
+    price: 2000,
+  },
+];
 
-const PurchasingHistory = ({
-  searchParams,
-}: {
-  searchParams: PageProps['searchParams'];
-}) => {
-  const { handleParamChange } = useQueryParams();
-  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
-
-  async function getUserInfo() {
-    const res = await productService.getUserDetails();
-    // Process response
-  }
-
-  useEffect(() => {
-    const fetchedUserInfo = authUtils.getUserInfo();
-    setUserInfo(fetchedUserInfo);
-  }, []);
-
-  const orders = [0, 0, 0, 0, 0, 0].map((item, i) => <PurchaseItem key={i} />);
-
+export default function PurchasingHistory() {
+  const [orders, setOrders] = useState(ordersData);
+  const { region } = useRegion();
   return (
-    <Tabs.Root
-      defaultValue={searchParams.tab || 'completed'}
-      onValueChange={(value) => handleParamChange('tab', value)}
-    >
-      <Tabs.List
-        className='flex flex-col items-center gap-5 md:flex-row'
-        aria-label='Purchasing history'
-      >
-        <h1 className='text-3xl font-medium'>Purchasing History</h1>
-        <div className='flex gap-2'>
-          <Tabs.Trigger
-            value='completed'
-            className='rounded-full border p-2 px-5 focus-visible:outline-primary data-[state=active]:border-primary data-[state=active]:bg-primary/20'
-          >
-            Completed Orders
-          </Tabs.Trigger>
-          <Tabs.Trigger
-            value='pending'
-            className='rounded-full border p-2 px-5 focus-visible:outline-primary data-[state=active]:border-primary data-[state=active]:bg-primary/20'
-          >
-            Pending Orders
-          </Tabs.Trigger>
+    <div>
+      {orders.length < 1 ? (
+        <div className='flex min-h-[80vh] items-center justify-center'>
+          <NoHistory title='No Purchase History Yet' />
         </div>
-      </Tabs.List>
-      <Tabs.Content value='completed' className='py-5'>
-        {/* {orders} */}
-        <div className='py-32'>
-          <NoHistory />
-        </div>
-      </Tabs.Content>
-      <Tabs.Content value='pending' className='py-5'>
-        {orders}
-      </Tabs.Content>
-    </Tabs.Root>
-  );
-};
+      ) : (
+        <div className='lg:mt-10 grid gap-5'>
+          {orders.map((order, i) => (
+            <div
+              key={i}
+              className='flex max-w-[957px] items-center gap-4 rounded-[20px] bg-[#F4F4F4]/95 px-3 py-4 lg:gap-10 lg:rounded-[28px] lg:bg-[#F4F4F4] lg:px-9 lg:py-[30px]'
+            >
+              <Image
+                className='h-[75px] w-[75px] rounded-[15px] object-contain lg:h-[95px] lg:w-[95px] lg:rounded-3xl'
+                src='/images/product/save.png'
+                alt=''
+                width={95}
+                height={95}
+              />
+              <div className='lg:hidden grid gap-1'>
+                <p className='mb-1 max-w-[475px] text-xs text-black'>
+                  {order.product}
+                </p>
+                <ProductPrice
+                  priceInNGN={order.price}
+                  region={region}
+                  className='font-clashmd text-xs text-black'
+                />
+                <div className='flex items-center justify-between'>
+                  <p className='text-[10px] text-black'>
+                    Order ID : {order.id}
+                  </p>
+                  <p
+                    className={cn('w-fit rounded-full px-[10px] py-[5px]', {
+                      'bg-[#F8BCBC] font-clashmd text-[8px] text-[#8B1A1A]':
+                        order.status === 'Completed',
+                      'bg-[#BAD9F7] font-clashmd text-[8px] text-[#1673CC]':
+                        order.status === 'Pending',
 
-const PurchaseItem = () => {
-  return (
-    <div className='mb-5 grid max-w-4xl gap-3 rounded-xl border-b border-gray-100 bg-gray-100 px-4 py-5 md:bg-white'>
-      <div className='flex justify-between gap-3'>
-        <div className='flex gap-3'>
-          <div className='shrink-0'>
-            <Image
-              className='rounded-xl'
-              src='/images/product/samsung-galaxy.png'
-              alt=''
-              width='90'
-              height='100'
-            />
-          </div>
-          <div className='grid gap-2'>
-            <p>
-              Samsung Galaxy A14 6.6&quot; 4GB RAM/64GB ROM Android 13 - Light
-              Green
-            </p>
-            <span className='font-bold md:hidden'>₦ 3,850</span>
-            <div className='hidden items-center gap-2 text-xs md:flex'>
-              <span>Brand: </span>
-              <span className='rounded-full bg-primary/20 p-3'>Samsung</span>
+                      'bg-[#BAF7BA] font-clashmd text-[8px] text-[#1B691B]':
+                        order.status === 'Ongoing',
+                    })}
+                  >
+                    {order.status}
+                  </p>
+                </div>
+              </div>
+              <div className='hidden w-full justify-between lg:flex'>
+                <div>
+                  <p className='mb-1 max-w-[475px] text-base text-black'>
+                    {order.product}
+                  </p>
+                  <div className='flex items-center gap-4'>
+                    <p className='text-sm text-black'>Order ID : {order.id}</p>
+                    <p
+                      className={cn('w-fit rounded-full px-5 py-2', {
+                        'bg-[#F8BCBC] text-[10px] text-[#8B1A1A] lg:text-sm':
+                          order.status === 'Completed',
+                        'bg-[#BAD9F7] text-[10px] text-[#1673CC] lg:text-sm':
+                          order.status === 'Pending',
+
+                        'bg-[#BAF7BA] text-[10px] text-[#1B691B] lg:text-sm':
+                          order.status === 'Ongoing',
+                      })}
+                    >
+                      {order.status}
+                    </p>
+                  </div>
+                </div>
+                <div className='grid gap-5'>
+                  <ProductPrice
+                    priceInNGN={order.price}
+                    region={region}
+                    className='font-clashmd text-xl text-black'
+                  />
+                  <Link href='/' className='text-sm text-primary'>
+                    See Details
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-        <div className='flex min-w-fit flex-col items-end justify-end md:justify-between'>
-          <span className='hidden font-bold md:block'>₦ 3,850</span>
-          <Link href='' className='text-sm text-primary'>
-            See details
-          </Link>
-        </div>
-      </div>
+      )}
     </div>
   );
-};
-
-export default PurchasingHistory;
+}

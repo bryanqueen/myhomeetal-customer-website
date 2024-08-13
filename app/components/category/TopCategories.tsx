c2cff3f2ebebe59f1e441faf58861c6112b5efae
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-08-13 23:39:16 +0100

    update

--- app/components/category/TopCategories.tsx
+++ app/components/category/TopCategories.tsx
@@ -37,7 +37,7 @@ const TopCategories = ({ topCategories }: any) => {
                   <div className='flex items-center justify-center lg:w-[132px] lg:h-[132px] rounded-full'>
                     <Image
                       className='w-[78px] h-[78px] lg:h-[132px] lg:w-[132px] rounded-full object-cover p-2 transition'
-                      src={category.product_category_image || '/images/confetti.png'}
+                      src={category.product_category_image || '/placeholder.png'}
                       alt='Top product'
                       width={132}
                       height={132}

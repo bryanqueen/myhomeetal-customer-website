0867206a5511afd87b992ba9bafdd9978f451cc9
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-09-17 09:17:58 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,10 @@ export const metadata: Metadata = {
   title: 'MYHOMEETAL - Your one stop shop',
   description:
     'Welcome to MyHomeEtAl - Your one-stop online shop for everything you need. Shop our extensive range of products, including the latest tech, luxury skincare, and home decor. Enjoy unbeatable prices, high-quality products, and exceptional customer service. Transform your space and indulge yourself with MyHomeEtAl.',
+     // Add Google verification meta tag
+  other: {
+    'google-site-verification': 'xY6atIcfgkBRJeb0a4RqeJCghSo1oxL93JS1eV8GxKk',
+  },
 };
 
 export default function RootLayout({

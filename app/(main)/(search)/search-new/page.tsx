45fbb7f386cfdc6731f0f886085166193e47657e
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-11-04 04:55:41 +0100

    update

---
+++ app/(main)/(search)/search-new/page.tsx
@@ -0,0 +1,12 @@
+import SearchComponent from '@/app/components/SearchComponent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='min-h-screen px-[3%]'>
+      <SearchComponent />
+    </main>
+  )
+}
+
+export default page
---
+++ app/components/SearchComponent.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { index } from '@/algoliaClient';
+import { SearchNormal } from 'iconsax-react';
+import { FaSearchengin } from 'react-icons/fa6';
+import { InstantSearch, SearchBox, Hits, Pagination } from 'react-instantsearch-dom';
+
+function SearchComponent() {
+  return (
+    <InstantSearch searchClient={index} indexName="products">
+      <SearchBox
+      translations={{ placeholder: "What can we help you find?" }}
+      submitIconComponent={<SearchNormal className="text-2xl size-20" />}
+      className="custom-search" // Optional: to apply any outer container styling
+    />
+      <Hits hitComponent={Product} />
+      <Pagination />
+    </InstantSearch>
+  );
+}
+
+function Product({ hit }) {
+  return (
+    <div>
+      <h2>{hit.productTitle}</h2>
+      <p>{hit.description}</p>
+      <p>${hit.price}</p>
+    </div>
+  );
+}
+
+export default SearchComponent;

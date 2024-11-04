fa413ee208cfd3e0d0a05523d313ba860c015f7f
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-11-04 05:03:16 +0100

    update

--- app/components/SearchComponent.tsx
+++ app/components/SearchComponent.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { index } from '@/algoliaClient';
 import { SearchNormal } from 'iconsax-react';
-import { FaSearchengin } from 'react-icons/fa6';
 import { InstantSearch, SearchBox, Hits, Pagination } from 'react-instantsearch-dom';
 
 function SearchComponent() {

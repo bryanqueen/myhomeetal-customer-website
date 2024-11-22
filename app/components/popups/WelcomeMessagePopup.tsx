bd32fc9645e6ad44b3dc2400fd27847a5b7990f9
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-11-22 14:38:42 +0100

    fix

--- app/components/popups/WelcomeMessagePopup.tsx
+++ app/components/popups/WelcomeMessagePopup.tsx
@@ -23,7 +23,7 @@ const WelcomeMessagePopup = () => {
       document.body.style.overflow = "hidden"
       setTimeout(() => {
         setIsShowPopup(true);
-      }, 1500);
+      }, 500);
     }
   }, [showPopup]);
 

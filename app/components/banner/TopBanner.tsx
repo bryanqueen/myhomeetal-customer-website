b169d3b562cc25bcc391cacfeecdc110502bb7a5
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-12-18 16:32:41 +0100

    change text

--- app/components/banner/TopBanner.tsx
+++ app/components/banner/TopBanner.tsx
@@ -4,7 +4,7 @@ const TopBanner = () => {
   return (
     <div className='hidden relative z-50 h-[43px] items-center justify-center bg-primaryBg text-center text-sm text-white lg:flex'>
       <p>
-        Myhomeetal.com.ng is absolutely your one-stop store for genuine and
+        myhomeetal.com is absolutely your one-stop store for genuine and
         affordable products! -{' '}
         <Link href='/about' className='font-clashmd'>Learn more about us</Link>
       </p>
